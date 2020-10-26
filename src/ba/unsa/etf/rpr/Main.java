84bb9ea1fdb591c5dd2730a2d35defe82404c6cb
msladic1 <msladic1@etf.unsa.ba>
2020-10-26 18:22:32 +0100

    Napisana funkcija sumaCifara

--- src/ba/unsa/etf/rpr/Main.java
+++ src/ba/unsa/etf/rpr/Main.java
@@ -1,8 +1,17 @@
 package ba.unsa.etf.rpr;
 
+import java.util.Scanner;
 public class Main {
-
+    static int sumaCifara(int n) {
+        int suma = 0, cifra, pom = n;
+        while(pom != 0) {
+            cifra = pom%10;
+            suma = suma + cifra;
+            pom = pom/10;
+        }
+        return suma;
+    }
     public static void main(String[] args) {
-    
+
     }
 }
